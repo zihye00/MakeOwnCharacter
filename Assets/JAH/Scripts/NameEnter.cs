b54b476af01ebb07c124066f7e8762f94fe37aab
DESKTOP-DPI5LD8\iibi <ahyeon621@naver.com>
2023-06-28 16:57:28 +0900

    마우스 클릭, 파티클x...returnx

--- Assets/JAH/Scripts/NameEnter.cs
+++ Assets/JAH/Scripts/NameEnter.cs
@@ -1,24 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 // 역할 1 : 이름을 입력하고 엔터를 누르면 커스텀 UI 제거
 // - 커스텀 UI
+
 // 역할 2:  카메라 UI 생성
 // - 카메라 UI
+
+// 역할 3 : Midair인식기를 지운다
+// - Midair인식
 
 public class NameEnter : MonoBehaviour
 {
     // - 커스텀 UI
     public GameObject customUI;
-    // - 카메라 UI
-    public GameObject cameraUI;
+    // - 카메라 On 버튼
+    public GameObject OnBtn;
+    // - Midair인식
+    public GameObject midairFinder;
 
     public void NameInput()
     {
         // 역할 1 : 이름을 입력하고 엔터를 누르면 커스텀 UI 제거
         Destroy(customUI);
         // 역할 2:  카메라 UI 생성
-        cameraUI.gameObject.SetActive(true);
+        OnBtn.gameObject.SetActive(true);
+        // 역할 3 : Midair인식기를 지운다
+        // Plan Finder의 앵커스테이지에서 Ground Plan Stage 제거
+        MidAirPositionerBehaviour.Destroy(midairFinder);
     }
 }
--- Assets/JAH/Scripts/TouchManager.cs
+++ Assets/JAH/Scripts/TouchManager.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Vuforia;
 
-// 역할  : 캐릭터을 터치하면 나 자신만 활성화
+// 역할 1 : 캐릭터을 터치하면 나 자신만 활성화
 // - 나머지 캐릭터들 비활성화
 
+// 역할 2 : 터치 이펙트 추가
+// 터치할 때마다 paricle이 나오게 한다
+// - 별 파티클
+// - 시간 텀
 
 public class TouchManager : MonoBehaviour
 {
@@ -15,12 +19,24 @@ public class TouchManager : MonoBehaviour
     public GameObject[] characters;
     // - 커스텀 UI
     public GameObject customUI;
+    // - ar 카메라
     public Camera cam;
-    public GameObject planFinder;
+    // - Midair 인식
+    public GameObject midairFinder;
+
+
+    // 역할 2 : 터치 이펙트 추가
+    // -   파티클
+    public GameObject effectPref;
+    // - 시간 텀
+    private float spawnsTime;
+    public float defaultTime = 0.05f;
 
     private void Update()
     {
+        spawnsTime += Time.deltaTime;
         Touch();
+        MouseClick();
     }
     // 역할 1 : Character_1(나 자신)을 터치하면 나 자신만 활성화
     public void Touch()
@@ -35,7 +51,7 @@ public class TouchManager : MonoBehaviour
         if(gameObject.activeSelf == true)
         {
             // Plan Finder의 앵커스테이지에서 Ground Plan Stage 제거
-            MidAirPositionerBehaviour.Destroy(planFinder);
+            MidAirPositionerBehaviour.Destroy(midairFinder);
 
             if (touch.phase == TouchPhase.Began)
             {
@@ -49,35 +65,49 @@ public class TouchManager : MonoBehaviour
                     if (hitInfo.transform.name == "Character_1")
                     {
                         // 나머지 캐릭터들 없애기
-                        Destroy(characters[1]);
-                        Destroy(characters[2]);
-                        Destroy(characters[3]);
+                        //Destroy(characters[1]);
+                        //Destroy(characters[2]);
+                        //Destroy(characters[3]);
 
                         //hitInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                         //cam.transform.localEulerAngles = hitInfo.transform.localEulerAngles;
+                        particleActive();
+
+                        // 나머지 캐릭터들 비활성화
+                        characters[1].gameObject.SetActive(false);
+                        characters[2].gameObject.SetActive(false);
+                        characters[3].gameObject.SetActive(false);
 
                         CheckIndex.Instance.SetCharacterIndex("Character1");
 
                         // 2초 후 커스텀UI 나옴
                         StartCoroutine(CharacterCustom());
+
                     }
 
                     // 캐릭터2 를 터치하면
                     if (hitInfo.transform.name == "Character_2")
                     {
                         // 나머지 캐릭터들 없애기
-                        Destroy(characters[0]);
-                        Destroy(characters[2]);
-                        Destroy(characters[3]);
+                        //Destroy(characters[0]);
+                        //Destroy(characters[2]);
+                        //Destroy(characters[3]);
 
                         //hitInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                         //cam.transform.localEulerAngles = hitInfo.transform.localEulerAngles;
+                        particleActive();
+
+                        // 나머지 캐릭터들 비활성화
+                        characters[0].gameObject.SetActive(false);
+                        characters[2].gameObject.SetActive(false);
+                        characters[3].gameObject.SetActive(false);
 
                         CheckIndex.Instance.SetCharacterIndex("Character2");
 
 
                         // 2초 후 커스텀UI 나옴
                         StartCoroutine(CharacterCustom());
+
                     }
 
 
@@ -85,12 +115,19 @@ public class TouchManager : MonoBehaviour
                     if (hitInfo.transform.name == "Character_3")
                     {
                         // 나머지 캐릭터들 없애기
-                        Destroy(characters[0]);
-                        Destroy(characters[1]);
-                        Destroy(characters[3]);
+                        //Destroy(characters[0]);
+                        //Destroy(characters[1]);
+                        //Destroy(characters[3]);
 
                         //hitInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                         //cam.transform.localEulerAngles = hitInfo.transform.localEulerAngles;
+
+                        particleActive();
+
+                        // 나머지 캐릭터들 비활성화
+                        characters[0].gameObject.SetActive(false);
+                        characters[1].gameObject.SetActive(false);
+                        characters[3].gameObject.SetActive(false);
 
                         CheckIndex.Instance.SetCharacterIndex("Character3");
 
@@ -104,19 +141,26 @@ public class TouchManager : MonoBehaviour
                     if (hitInfo.transform.name == "Character_4")
                     {
                         // 나머지 캐릭터들 없애기
-                        Destroy(characters[0]);
-                        Destroy(characters[1]);
-                        Destroy(characters[2]);
+                        //Destroy(characters[0]);
+                        //Destroy(characters[1]);
+                        //Destroy(characters[2]);
 
                         //hitInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
                         //cam.transform.localEulerAngles = hitInfo.transform.localEulerAngles;
+
+                        particleActive();
+
+                        // 나머지 캐릭터들 비활성화
+                        characters[0].gameObject.SetActive(false);
+                        characters[1].gameObject.SetActive(false);
+                        characters[2].gameObject.SetActive(false);
 
                         CheckIndex.Instance.SetCharacterIndex("Character4");
 
                         // 2초 후 커스텀UI 나옴
                         StartCoroutine(CharacterCustom());
                     }
-
+  
                 }
 
             }
@@ -124,13 +168,162 @@ public class TouchManager : MonoBehaviour
                
 
         }
+    }
 
         IEnumerator CharacterCustom()
         {
             yield return new WaitForSeconds(2);
             customUI.SetActive(true);
         }
+
+    // 역할 2 : 터치 이펙트 추가
+    private void EffectParticle()
+    {
+
+        // 파티클이 나오는 위치.-> 첫번째 터치 위치
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        pos.z = 0;
+        Instantiate(effectPref, pos, Quaternion.identity);
     }
 
- 
+    // - 일정 시간텀을 두고 파티클이 나오도록
+    private void particleActive()
+    {
+        if (spawnsTime >= defaultTime)
+        {
+            EffectParticle();
+            spawnsTime = 0f;
+        }
+
+    }
+
+    // Return 버튼을 누르면 캐릭터들 다시 생성되도록
+    public void CharactersActive()
+    {
+        // 캐릭터4명이 다시 뜸
+        characters[0].gameObject.SetActive(true);
+        characters[1].gameObject.SetActive(true);
+        characters[2].gameObject.SetActive(true);
+        characters[3].gameObject.SetActive(true);
+
+        // CustomUI 비활성화
+        customUI.gameObject.SetActive(false);
+    }
+
+    private void MouseClick()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo = new RaycastHit();
+            if (Physics.Raycast(ray, out hitInfo, Mathf.Infinity))
+            {
+                Debug.Log("hitInfo : " + hitInfo);
+                // 캐릭터1 을 터치하면
+                if (hitInfo.transform.name == "Character_1")
+                {
+                    // 나머지 캐릭터들 없애기
+                    //Destroy(characters[1]);
+                    //Destroy(characters[2]);
+                    //Destroy(characters[3]);
+
+                    //hitInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    //cam.transform.localEulerAngles = hitInfo.transform.localEulerAngles;
+                    particleActive();
+
+                    // 나머지 캐릭터들 비활성화
+                    characters[1].gameObject.SetActive(false);
+                    characters[2].gameObject.SetActive(false);
+                    characters[3].gameObject.SetActive(false);
+
+                    CheckIndex.Instance.SetCharacterIndex("Character1");
+
+                    // 2초 후 커스텀UI 나옴
+                    StartCoroutine(CharacterCustom());
+
+                }
+
+                // 캐릭터2 를 터치하면
+                if (hitInfo.transform.name == "Character_2")
+                {
+                    // 나머지 캐릭터들 없애기
+                    //Destroy(characters[0]);
+                    //Destroy(characters[2]);
+                    //Destroy(characters[3]);
+
+                    //hitInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    //cam.transform.localEulerAngles = hitInfo.transform.localEulerAngles;
+                    particleActive();
+
+                    // 나머지 캐릭터들 비활성화
+                    characters[0].gameObject.SetActive(false);
+                    characters[2].gameObject.SetActive(false);
+                    characters[3].gameObject.SetActive(false);
+
+                    CheckIndex.Instance.SetCharacterIndex("Character2");
+
+
+                    // 2초 후 커스텀UI 나옴
+                    StartCoroutine(CharacterCustom());
+
+                }
+
+
+                // 캐릭터3 을 터치하면
+                if (hitInfo.transform.name == "Character_3")
+                {
+                    // 나머지 캐릭터들 없애기
+                    //Destroy(characters[0]);
+                    //Destroy(characters[1]);
+                    //Destroy(characters[3]);
+
+                    //hitInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    //cam.transform.localEulerAngles = hitInfo.transform.localEulerAngles;
+
+                    particleActive();
+
+                    // 나머지 캐릭터들 비활성화
+                    characters[0].gameObject.SetActive(false);
+                    characters[1].gameObject.SetActive(false);
+                    characters[3].gameObject.SetActive(false);
+
+                    CheckIndex.Instance.SetCharacterIndex("Character3");
+
+
+                    // 2초 후 커스텀UI 나옴
+                    StartCoroutine(CharacterCustom());
+                }
+
+
+                // 캐릭터4 를 터치하면
+                if (hitInfo.transform.name == "Character_4")
+                {
+                    // 나머지 캐릭터들 없애기
+                    //Destroy(characters[0]);
+                    //Destroy(characters[1]);
+                    //Destroy(characters[2]);
+
+                    //hitInfo.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                    //cam.transform.localEulerAngles = hitInfo.transform.localEulerAngles;
+
+                    particleActive();
+
+                    // 나머지 캐릭터들 비활성화
+                    characters[0].gameObject.SetActive(false);
+                    characters[1].gameObject.SetActive(false);
+                    characters[2].gameObject.SetActive(false);
+
+                    CheckIndex.Instance.SetCharacterIndex("Character4");
+
+                    // 2초 후 커스텀UI 나옴
+                    StartCoroutine(CharacterCustom());
+                }
+
+            }
+
+        }
+
+    }
 }
+
