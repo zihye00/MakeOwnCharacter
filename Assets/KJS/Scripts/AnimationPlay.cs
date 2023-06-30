4e29cdcf6894144347dafe37a77ee37303173f3d
junsun <gohg123@naver.com>
2023-06-30 17:40:43 +0900

    애니메이션 넣기

---
+++ Assets/KJS/Scripts/AnimationPlay.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlay : MonoBehaviour
+{
+    
+
+    public enum AnimeState
+    {
+        IDLE,
+        Hi,
+    }
+
+    // Animator
+    public Animator[] anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+   
+
+   
+
+    private void Idle()
+    {
+
+        
+
+        // + Animation 상태를 Idle 로 변경
+        anim[0].SetTrigger("IDLE");
+        anim[1].SetTrigger("IDLE");
+        anim[2].SetTrigger("IDLE");
+        anim[3].SetTrigger("IDLE");
+
+    }
+
+    
+    public void Hi()
+    {
+        // + Animation 상태를 HI 로 변경
+        anim[0].SetTrigger("HI"); 
+        anim[1].SetTrigger("HI");
+        anim[2].SetTrigger("HI");
+        anim[3].SetTrigger("HI");
+
+
+    }
+
+    
+}
